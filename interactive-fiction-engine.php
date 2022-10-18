47cfdde774af6e2c909ec646e2d3066c0ed6126b
Artemio Morales <artemio@Artemios-MBP.lan>
2022-10-18 11:38:02 -0400

    Clean up stylesheet enqueue and PHP config file

--- interactive-fiction-engine.php
+++ interactive-fiction-engine.php
@@ -30,20 +30,12 @@ add_action( 'init', 'create_block_interactive_fiction_engine_block_init' );
 
 
 
-// function interactive_fiction_engine_editor_enqueue() {
-// 	wp_enqueue_style( 'interactive_fiction_engine-style',
-// 		plugins_url( 'assets/editor-style.css', __FILE__ )
-// 	);
-//     wp_enqueue_script( 'interactive_fiction_engine-script',
-//         plugins_url( 'assets/interactive-fiction-engine.js', __FILE__ ),
-//         array( 'wp-blocks' )
-//     );
-// 	wp_enqueue_script( 'interactive_fiction_engine-initialize',
-//         plugins_url( 'assets/initialize.js', __FILE__ ),
-//         array( 'wp-blocks', 'interactive_fiction_engine-script' )
-//     );
-// }
-// add_action( 'enqueue_block_editor_assets', 'interactive_fiction_engine_editor_enqueue' );
+function interactive_fiction_engine_editor_enqueue() {
+	wp_enqueue_style( 'interactive_fiction_engine-editor-style',
+		plugins_url( 'assets/editor-style.css', __FILE__ )
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'interactive_fiction_engine_editor_enqueue' );
 
 
 function interactive_fiction_engine_general_enqueue() {
@@ -51,8 +43,7 @@ function interactive_fiction_engine_general_enqueue() {
 		plugins_url( 'assets/style.css', __FILE__ )
 	);
     wp_enqueue_script( 'interactive_fiction_engine-script',
-        plugins_url( 'assets/interactive-fiction-engine.js', __FILE__ ),
-        array( 'wp-blocks' )
+        plugins_url( 'assets/interactive-fiction-engine.js', __FILE__ )
     );
 }
 add_action( 'enqueue_block_assets', 'interactive_fiction_engine_general_enqueue' );
@@ -62,25 +53,16 @@ function interactive_fiction_engine_ui_scripts($hook) {
 
 	wp_enqueue_script( 'interactive_fiction_engine-dynamic-paragraph',
 		plugins_url( 'assets/dynamic-paragraph.js', __FILE__ ),
-		array( 'wp-blocks', 'interactive_fiction_engine-script' ),
+		array( 'interactive_fiction_engine-script' ),
 		'0.1',
 		true
 	);
 
 	wp_enqueue_script( 'interactive_fiction_engine-initialize',
         plugins_url( 'assets/initialize.js', __FILE__ ),
-        array( 'wp-blocks', 'interactive_fiction_engine-dynamic-paragraph' ),
+        array( 'interactive_fiction_engine-dynamic-paragraph' ),
 		'0.1',
 		true
     );
-
-	// $script_js = 'assets/interactive-fiction-engine.js';
-	// wp_enqueue_script( 'interactive-fiction-engine', plugins_url($script_js, __FILE__), [], '0.1', true);
 }
 add_action('wp_enqueue_scripts', 'interactive_fiction_engine_ui_scripts');
-
-
-// function interactive_fiction_engine_stylesheet() {
-//     wp_enqueue_style( 'interactive_fiction_engine-style', plugins_url( 'style.css', __FILE__ ) );
-// }
-// add_action( 'enqueue_block_assets', 'interactive_fiction_engine_stylesheet' );
