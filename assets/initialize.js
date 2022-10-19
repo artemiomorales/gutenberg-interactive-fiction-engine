75555c94d8537a6341268718491413266a761d4d
Artemio Morales <artemio@Artemios-MBP.lan>
2022-10-19 16:35:28 -0400

    Remove extraneous code in initialize.js

--- assets/initialize.js
+++ assets/initialize.js
@@ -1,15 +1,11 @@
-console.log('init script');
 window.ifEngine.dynamicParagraphs = [];
 const dynamicParagraphElements = document.querySelectorAll(".ifengine__choice");
 dynamicParagraphElements.forEach((element) => {
 	const dynamicParagraph = new DynamicParagraph(element);
 	window.ifEngine.registerListener(dynamicParagraph);
 	const links = dynamicParagraph.domElement.querySelectorAll('a');
-	console.log(links);
 	links.forEach((link, index) => {
 		link.addEventListener('click', (e) => {
-			// e.preventDefault();
-			console.log(dynamicParagraph.domElement.getAttribute('data-activecondition'));
 			if(parseInt(dynamicParagraph.domElement.getAttribute('data-activecondition')) !== index) {
 				dynamicParagraph.domElement.setAttribute('data-activecondition', index);
 			} else {
